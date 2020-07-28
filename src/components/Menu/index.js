f088876f14d2c7baa9eb9104d30eb64d60ac9427
Thais de Campos <thaisd.campos@gmail.com>
2020-07-28 19:48:17 -0300

    Router to register video and category

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/Logo.png';
 import './Menu.css';
-import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu() {
   return(
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="AluraFlix"/>
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
          Novo vídeo
       </Button>
     </nav>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain'
-import VideoCardGroup from './components/Carousel';
-import Footer from './components/Footer';
-import dadosIniciais from './data/dados_iniciais.json';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain'
+import VideoCardGroup from '../../components/Carousel';
+import Footer from '../../components/Footer';
+import dadosIniciais from '../../data/dados_iniciais.json';
 
-function App() {
+function Home() {
   return (
     <div>
       <Menu />
-
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-end?"}
       />
-
+      
       {
         dadosIniciais.categorias.map(categoria => 
           <VideoCardGroup 
@@ -24,10 +23,9 @@ function App() {
           />
         )
       }
-      
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
