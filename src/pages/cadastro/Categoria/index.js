6fe58170a0a3134940cffbac592fde477e0593b7
Thais de Campos <thaisd.campos@gmail.com>
2020-07-30 23:19:53 -0300

    Create server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 
-function CadastroCategoria () {
+function CadastroCategoria() {
   const initialValues = {
     name: '',
     desciption: '',
-    colorCategory: ''
-  }
+    colorCategory: '',
+  };
 
   const [categories, setCategories] = useState([]);
   const [values, setValues] = useState(initialValues);
@@ -17,84 +17,89 @@ function CadastroCategoria () {
     setValues({
       ...values,
       [key]: value, // nome: 'valor'
-    })
+    });
   }
 
   function handleChange(event) {
-    console.log("teste"+ event)
+    console.log(`teste${event}`);
     setValue(
       event.target.getAttribute('name'),
-      event.target.value
+      event.target.value,
     );
   }
 
-  return(
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategories(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
+  return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.name}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {' '}
+        {values.name}
+      </h1>
 
       <form onSubmit={function handleSubmit(event) {
         event.preventDefault();
         setCategories([...categories, values]);
-        setValues(initialValues)
-      }}>
-       
-      <FormField
-        label="Nome da Categoria"
-        type="text"
-        name="name"
-        value={values.name}
-        onChange={handleChange}
-      />
+        setValues(initialValues);
+      }}
+      >
 
-      {/* <FormField
-        label="Descrição"
-        type="textarea"
-        name="description"
-        value={values.description}
-        onChange={handleChange}
-      /> */}
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
 
-      { 
-      <div>
-        <label>
-          Descrição:
-          <textarea
-            type="text"
-            value={values.desciption}
-            name="description"
-            onChange={handleChange}
-          />
-        </label>
-      </div> 
-      }
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
 
-      <FormField
-        label="Cor"
-        type="color"
-        name="colorCategory"
-        value={values.colorCategory}
-        onChange={handleChange}
-      />
+        <FormField
+          label="Cor"
+          type="color"
+          name="colorCategory"
+          value={values.colorCategory}
+          onChange={handleChange}
+        />
 
-      <button>
-        Cadastrar
-      </button>
-    </form>
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
-    <ul>
-      {categories.map((category, index) => {
-        return(
-          <li key={`${category}${index}`}>
-            {category.name}
+      <ul>
+        {categories.map((category) => (
+          <li key={category.name}>
+            {category.titulo}
           </li>
-        )
-      })}
-    </ul>
+        ))}
+      </ul>
 
-    <Link to="/">
-      Ir para home
-    </Link>
-  </PageDefault>
-)}
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+}
 
-export default CadastroCategoria
+export default CadastroCategoria;
